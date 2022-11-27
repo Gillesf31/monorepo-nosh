b267732020bc46999fd39a9c09af73fd1ebdd116
Gilles <gilles.ferrand31@gmail.com>
2022-11-27 11:38:47 +0100

    Test observer-spy

--- apps/test-observer-spy/src/app/app.component.spec.ts
+++ apps/test-observer-spy/src/app/app.component.spec.ts
@@ -1,32 +1,44 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {delay, from, Observable, of} from 'rxjs';
+import {AppComponent} from './app.component';
+import {subscribeSpyTo} from '@hirez_io/observer-spy';
+
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AppComponent, NxWelcomeComponent],
-    }).compileComponents();
-  });
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AppComponent ]
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
-
-  it(`should have as title 'test-observer-spy'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-observer-spy');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome test-observer-spy'
-    );
+  describe('component doSomething function', () => {
+    test.each([
+      [[1, 2, 3], [2, 4, 6]],
+      [[1, 1, 1], [2, 2, 2]],
+      [[0, 0, 0], [0, 0, 0]],
+    ])(
+      "Given an observable with %p emitted values as arguments should returns %p",
+      (firstArg, expectedResult) => {
+        expect(subscribeSpyTo(component.doSomething(from(firstArg))).getValues()).toEqual(expectedResult);
+      })
+  })
+  describe('component doRace function',() => {
+    test.each`
+        a | expected
+        ${[of(3), of(2), of(1)]} | ${3}
+        ${[of(1), of(2), of(1)]} | ${1}
+        ${[of(4)]}                           | ${4}
+        ${[of('first'), of('second')]}                           | ${'first'}
+        ${[of('second').pipe(delay(200)), of('first').pipe(delay(300))]}                          | ${'second'}
+        ${[of(true), of(false)]}                           | ${true}
+      `('Given an array of observable as arguments should returns $expected',async({a, expected}) => {
+        const observerSpy = subscribeSpyTo(component.doRace(a));
+        await observerSpy.onComplete();
+        expect(observerSpy.getFirstValue()).toEqual(expected);
+      });
   });
 });
--- apps/test-observer-spy/src/app/app.component.ts
+++ apps/test-observer-spy/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {delay, map, Observable, of, race} from 'rxjs';
 
 @Component({
   selector: 'monorepo-nosh-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'test-observer-spy';
+export class AppComponent implements OnInit {
+  obs1$: Observable<number> = of(1, 2, 3);
+  obs2$: Observable<number> = of(4, 5, 6);
+  obs3$: Observable<number> = of(7, 8, 9);
+
+  ngOnInit(): void {
+    race([of('second').pipe(delay(200)), of('first').pipe(delay(300))]).subscribe(console.warn);
+  }
+
+  doSomething(obs$: Observable<number>): Observable<number> {
+    return obs$.pipe(
+      map((value) => value * 2)
+    );
+  }
+
+  doRace<T>(observables$: Array<Observable<T>>): Observable<T> {
+    return race(observables$);
+  }
 }
